b4bd41280e297a259502f28cfa1ba2d97b9506f8
kevinhowbrook <kbhowbrook@gmail.com>
2017-11-12 15:28:43 +0000

    corrected the loop functions to accout for columns and rows in square itterations, added a calculated border

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import re
 import math
 
 def convert():
-    # with open('text.txt', 'r') as myfile:
-    #     input = myfile.read().replace('\n', '')
-    input = re.sub(r'([^\s\w]|_)+', '', 'one two three four five')
+    with open('input.txt', 'r') as myfile:
+        input = myfile.read().replace('\n', '')
+    input = re.sub(r'([^\s\w]|_)+', '', input)
     input = input.lower()
     input = input.split()
     alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",""]
@@ -64,6 +64,7 @@ def convert():
 
         colorList.append(colors)
 
+
     return colorList
 
 # get a list of possible squared numbers, this give us the number of
@@ -80,19 +81,19 @@ def grid(n,m):
     word_count = len(n)
     for i,m in enumerate(m):
         if m - word_count > 0 or m - word_count == 0:
-            return m
+            return float(m)
             break
 
 m = grid(convert(), calculate_m())
-
+print m,'x',m,'grid size'
 
 # length of canvas devide by square of m gives width or number of cols.
 # this gives us the width and height of a square on the canvas
 def square_size(m, canvas_size):
     #default 1000 1000
     canvas_sqrt = int(math.sqrt(m))
-    w = canvas_size / canvas_sqrt
-    h = canvas_size / canvas_sqrt
+    w = float(canvas_size) / float(canvas_sqrt)
+    print 'Square width and height: ', float(w)
     return w
 
 #print(square_size(m,1000)) # for 9 words return will be 333
@@ -113,15 +114,31 @@ def square_size(m, canvas_size):
 
 def draw():
     color = convert()
-    count = 0
     canvas_size = 1000
+    squares_per_row = canvas_size / square_size(m,canvas_size)
     im = Image.new('RGB', (canvas_size,canvas_size), color='white')
     draw = ImageDraw.Draw(im)
+    s = float(square_size(m,canvas_size))
+
+    #work out a suitable border width
+    border_width = int((float(30) * float(s)) / 100)
+
+    # if there are 3 squares to a row, we need to count 3 and increase
+    # the starting drawing point in the order 0,333,666,999
+    j = 0
+    k = 0
     for i,v in enumerate(convert()):
-        s = square_size(m,canvas_size)
-        points = (0,0), (0, s),(s,s),(s, 0)
-        print(points)
-        draw.polygon(points, outline='black',  fill = (v[0],v[1],v[2])) # outline='red', fill='blue'
+        # if i is a multiple of squares_per_row, count ++
+        if i % squares_per_row == 0 and i != 0:
+            j += 1
+        if i % squares_per_row == 0 and i != 0:
+            k = 0
+        points = ((k*s,j*s), (k*s, j*s+s),(k*s+s,j*s+s),(k*s+s, j*s))
+        draw.polygon((points[0], points[1], points[2], points[3]), outline='black',  fill = (v[0],v[1],v[2])) # outline='red', fill='blue'
+        #borders so redraw the same plots with white lines
+        draw.line((points[0], points[1], points[2], points[3], points[0]),  fill="white", width=border_width) # outline='red', fill='blue'
+        k += 1
+
     im.save('square.jpg')
 
 draw()
