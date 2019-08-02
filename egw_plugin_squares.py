0514d0810d703f451900e75c74b556f26fd2a7e5
s3h10r <sven.hessenmueller@gmail.com>
2019-08-03 01:30:29 +0200

    finishes migration to class-based plugins

--- egw_plugin_squares.py
+++ egw_plugin_squares.py
@@ -1,74 +1,58 @@
 #!/usr/bin/env python
 #coding=utf-8
-
-# === einguteswerkzeug plugin-interface ===
-# --- all einguteswerkzeug-plugins (generators, filters) must implement this
 import logging
 import random
 import string
+import sys
+from PIL import Image, ImageDraw
 from einguteswerkzeug.helpers import get_resource_file
-
-name = "squares+circles"
-description = "text in picture out"
-kwargs = {'textfile' : __file__,
-          'shape' : random.randint(0,1), 'svmode' : 1, 'ovmode' : 0,
-          'size'  : 800,
-          'font'  : get_resource_file('fonts/default.ttf')}
-args=None
-author = "Original (c) 2017 Kevin Howbrook. Fork, refactoring & extension (c) 2019 Sven Hessenmüller"
-version = '0.1.4'
-__version__ = version
+from einguteswerkzeug.plugins import EGWPluginGenerator
 
 # --- configure logging
 log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 handler = logging.StreamHandler() # console-handler
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 log.addHandler(handler)
 # ---
 
+meta = {
+    "name" : "squares+circles",
+    "version" : "0.1.6",
+    "description" : "text in picture out",
+    "author" : "Original (c) 2017 Kevin Howbrook. Fork, refactoring & extension (c) 2019 Sven Hessenmüller"
+}
 
-def run(textfile = None, shape = 1, svmode = 1, ovmode = 0, size = 800, font=None):
-    """
-    this is the wrapper around the functionality of the plugin.
-    """
-    log.info("textfile: %s" % textfile)
-    log.info("font: %s" % font)
+class SquaresAndCircles(EGWPluginGenerator):
+    def __init__(self, **kwargs):
+        super().__init__(**meta)
+        # defining mandatory kwargs (addionals to the mandatory of the base-class)
+        add_plugin_kwargs = { 'textfile' : __file__,
+                              'shape' : random.randint(0,1),
+                              'svmode' : 1, 'ovmode' : 0,
+                              'font'  : get_resource_file('fonts/default.ttf')}
+        super()._define_mandatory_kwargs(self, **add_plugin_kwargs)
+        self.kwargs = kwargs
 
-    ga = Square(title = "", mode = 1, file = textfile,
-                shape = shape, svmode = svmode, ovmode = ovmode,
-                image_size = size, font = font)
-    ga.draw()
-    return ga.get_image(), "generator %s v%s" % (name, __version__)
+    def _generate_image(self):
+        return _create_image(**self.kwargs)
 
-# --- END all einguteswerkzeug-plugins (generators, filters) must implement this
 
-def get_plugin_doc(format='text'):
-    """
-    """
-    if format not in ('txt', 'text', 'plaintext'):
-        raise Exception("Sorry. format %s not available. Valid options are ['text']" % format)
-    tpl_doc = string.Template("""
-    filters.$name - $description
-    kwargs  : $kwargs
-    args    : $args
-    author  : $author
-    version : __version__
-    """)
-    return tpl_doc.substitute({
-        'name' : name,
-        'description' : description,
-        'kwargs' : kwargs,
-        'args'    : args,
-        'author'  : author,
-        'version' : __version__,
-        })
 
-if __name__ == '__main__':
-    print(get_plugin_doc())
-
-# === END einguteswerkzeug plugin-interface
+generator = SquaresAndCircles()
+assert isinstance(generator,EGWPluginGenerator)
 
 # --- .. here comes the plugin-specific part to get some work done...
+
 from .squares import Square
+
+
+def _create_image(textfile = None, shape = 1, svmode = 1, ovmode = 0, size = (800,800), font=None):
+    log.info("textfile: %s" % textfile)
+    log.info("font: %s" % font)
+    ga = Square(title = "", mode = 1, file = textfile,
+                shape = shape, svmode = svmode, ovmode = ovmode,
+                image_size = size[0], font = font)
+    ga.draw()
+    return ga.get_image()
